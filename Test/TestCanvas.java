212a25b1a0759faa1ce819347eee429a08f76d15
Harsha <harshaskm @gmail.com>
2017-09-28 15:25:28 +0800

    Changes to objectify the code so that it is not like a script that uses Java

--- Test/TestCanvas.java
+++ Test/TestCanvas.java
@@ -9,4 +9,10 @@ public class TestCanvas {
 
         Assert.assertTrue(true);
     }
+
+//    @Test
+//    public void testToString() {
+//        Canvas c = new Canvas(1, 1, 5, 5, 3, 3, ' ', new char[5][5]);
+//        Assert.assertEquals("Hello", c.toString());
+//    }
 }
--- src/com/Canvas.java
+++ src/com/Canvas.java
@@ -1,29 +1,22 @@
 package com;
 
 public class Canvas {
-    private int horizontalBorderLines;
-    private int verticalBorderLines;
     private int canvasWidthWithBorder;
     private int canvasHeightWithBorder;
-    private int canvasHeightWithoutBorder;
-    private int canvasWidthWithoutBorder;
     private int headerLine;
     private int footerLine;
     private char symbol;
-    private char[][] canvas = new char[canvasHeightWithBorder][canvasWidthWithBorder];
+    private char[][] canvas;
 
-    public Canvas(int horizontalBorderLines, int verticalBorderLines, int canvasWidthWithBorder, int canvasHeightWithBorder
-                    , int canvasHeightWithoutBorder, int canvasWidthWithoutBorder, char symbol, char[][] canvas) {
-        this.horizontalBorderLines = horizontalBorderLines;
-        this.verticalBorderLines = verticalBorderLines;
-        this.canvasWidthWithBorder = canvasWidthWithBorder;
-        this.canvasHeightWithBorder = canvasHeightWithBorder;
-        this.canvasHeightWithoutBorder = canvasHeightWithoutBorder;
-        this.canvasWidthWithoutBorder = canvasWidthWithoutBorder;
+    public Canvas(int canvasHeightWithoutBorder, int canvasWidthWithoutBorder, char symbol) {
+        int horizontalBorderLines = 2;
+        int verticalBorderLines = 2;
+        this.canvasWidthWithBorder = canvasWidthWithoutBorder + horizontalBorderLines;
+        this.canvasHeightWithBorder = canvasHeightWithoutBorder + verticalBorderLines;
         this.headerLine = 0;
         this.footerLine = canvasHeightWithBorder - 1;
         this.symbol = symbol;
-        this.canvas = canvas;
+        this.canvas = new char[canvasHeightWithBorder][canvasWidthWithBorder];
 
         this.initializeCanvas();
     }
@@ -56,22 +49,7 @@ public class Canvas {
     }
 
     public static void print(int canvasWidthWithoutBorder, int canvasHeightWithoutBorder){
-        String lineOut="";
-        int horizontalBorderLines = 2;
-        int verticalBorderLines = 2;
-        int canvasWidthWithBorder = canvasWidthWithoutBorder + verticalBorderLines;
-        int canvasHeightWithBorder = canvasHeightWithoutBorder + horizontalBorderLines;
-        char[][] box = new char[canvasHeightWithBorder][canvasWidthWithBorder];
-
-        Canvas canvas = new Canvas(horizontalBorderLines, verticalBorderLines, canvasWidthWithBorder
-                                    , canvasHeightWithBorder, canvasHeightWithoutBorder, canvasWidthWithoutBorder, ' ', box);
-
-//        for(int height=0; height <= canvasHeightWithBorder; height++){
-//            for(int width=0; width <= canvasWidthWithBorder; width++){
-//                lineOut += canvas.canvas[width][height];
-//            }
-//        }
-
+        Canvas canvas = new Canvas(canvasHeightWithoutBorder, canvasWidthWithoutBorder, ' ');
         for(char[] row : canvas.canvas) {
             for (char cell : row) {
                 System.out.print(cell);

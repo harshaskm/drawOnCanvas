ca4ca7770ac2769236d5adb04f6e02c47dcb2756
Harsha <harshaskm @gmail.com>
2017-09-29 07:51:38 +0800

    Renamed a bunch of methods to remove redundant words

--- test/TestCanvas.java
+++ test/TestCanvas.java
@@ -7,9 +7,9 @@ public class TestCanvas {
     public void testPrintCanvas() {
 
         Canvas canvas = new Canvas(4, 20, ' ');
-        canvas.drawHorizontalLineInCanvas(1, 2, 6, 2);
-        canvas.drawVerticalLineInCanvas(6, 3, 6, 4);
-        canvas.createSmallBoxInTheCanvas(14,1,18,3);
+        canvas.drawHorizontalLine(1, 2, 6, 2);
+        canvas.drawVerticalLine(6, 3, 6, 4);
+        canvas.createSmallBox(14,1,18,3);
         System.out.println(canvas.toString());
 
         Assert.assertTrue(true);
